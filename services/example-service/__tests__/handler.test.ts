aac4b9f921b5e2d76679d7b74d7a9b2906fc4768
Lee Robert <lrobert@leerobert.ca>
2024-01-30 12:06:46 -0500

    fix(example-service): add new test

--- services/example-service/__tests__/handler.test.ts
+++ services/example-service/__tests__/handler.test.ts
@@ -4,6 +4,6 @@ test('handler should return expected message', async () => {
     expect(handler()).toEqual('Hello world! - Example Service - somePackage');
 });
 
-// test('nx should fail on  failure', async () => {
-//     fail('PLEASE EXIT WITH NON ZERO')
-// });
+test('nx should fail on  failure', async () => {
+    fail('PLEASE EXIT WITH NON ZERO')
+});
